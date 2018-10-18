b9612c3cda40ee367af143ca9bf3771b0c3fa07a
Christopher Deventer <deventer@ver2.de>
2018-10-18 17:14:19 +0200

    added hot reloading for styles

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './react/App';
-import './styles.less'
+import './styles.less';
 
 ReactDOM.render(<App />, document.getElementById('app'));
 /**
