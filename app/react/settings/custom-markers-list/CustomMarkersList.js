7e56476cda76d541109d7ded4f98453a9db41c47
Christopher Deventer <deventer@ver2.de>
2018-10-25 23:34:02 +0200

    added both preset markers list and custom markers list

---
+++ app/react/settings/custom-markers-list/CustomMarkersList.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {observable} from 'mobx';
+import {inject, observer} from 'mobx-react';
+import Store from '../../Store';
+import EditableMarkerList from '../editable-marker-list/EditableMarkerList';
+
+@inject('store')
+@observer
+export class CustomMarkersList extends React.Component{
+
+    @observable customMarkers = [];
+
+    componentDidMount() {
+        this.loadMarkers();
+    }
+
+    render() {
+        return (
+            <div  id="settingsManualList" className="customMarkersList">
+                <EditableMarkerList markers={this.customMarkers}/>
+            </div>  
+        );
+    }
+
+    loadMarkers() {
+        let {store} = {...this.props};
+        store.stores.modules
+            .find({type: 'manual'})
+            .sort({index: 1})
+            .then(results => {
+                this.customMarkers = results;
+            });
+    }
+}
+
+CustomMarkersList.propTypes = {
+    store: PropTypes.instanceOf(Store)
+};
---
+++ app/react/settings/preset-markers-list/PresetMarkersList.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {observable} from 'mobx';
+import EditableMarkerList from '../editable-marker-list/EditableMarkerList';
+import PropTypes from 'prop-types';
+import Store from '../../Store';
+import {inject, observer} from 'mobx-react';
+
+@inject('store')
+@observer
+export default class PresetMarkersList extends React.Component{
+    @observable presetMarkers = [];
+
+    componentDidMount() {
+        this.loadMarkers();
+    }
+
+    render() {
+        return (
+            <div  id="settingsManualList" className="presetMarkersList">
+                <EditableMarkerList markers={this.presetMarkers}/>
+            </div>
+        );
+    }
+
+    loadMarkers() {
+        let {store} = {...this.props};
+        store.stores.modules
+            .find({type: 'din'})
+            .sort({label: 1})
+            .then(results => {
+                this.presetMarkers = results;
+            });
+    }
+}
+
+PresetMarkersList.propTypes = {
+    store: PropTypes.instanceOf(Store)
+};
